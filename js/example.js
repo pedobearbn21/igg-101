fa3e1e2b97624d05f50c6bf75cce2c75ce76c03e
PedobearEarn <39798687+pedobearbn21@users.noreply.github.com>
2020-09-16 13:44:58 +0700

    Update example.js

--- js/example.js
+++ js/example.js
@@ -1,5 +1,9 @@
-console.log("Hello world");
-let name = "DopeDopeDope"
-name = "FourArms"
-const pi = 3.14;
-console.log("Pi :",pi,name);
+let me = {
+  name: 'sirasit',
+  surname: 'sanprom',
+  nickname: 'fluck',
+  age: 20,
+  hobby: "listening music",
+  tel: 0821848102
+}
+console.log(me);
