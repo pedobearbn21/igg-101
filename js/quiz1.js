28bddc79b4462fe05e406b7f1bd7a03253f14cbb
notnot123 <57088926+notnot123@users.noreply.github.com>
2020-09-16 15:53:46 +0700

    Create quiz1.js

---
+++ js/quiz1.js
@@ -0,0 +1,9 @@
+let profile = {
+    name :'Naphatsakorn',
+    Sname : 'Sajkhamfu',
+    Nname : 'James',
+    age : 21,
+    Hobbies : 'Listening Music',
+    PhoneNum : 091234567
+}
+console.log(profile)
