e7f32169a660edda7cfc8ddea56c7b5304bca82d
notnot123 <57088926+notnot123@users.noreply.github.com>
2020-09-16 14:50:50 +0700

    quiz2

---
+++ js/quiz2.js
@@ -0,0 +1,17 @@
+// ..1
+for(let i = 1; i<=20 ; i+=1){
+    console.log(i)
+}
+
+// ..2
+for(let i = 2; i<=20 ; i+=2){
+    console.log(i)
+}
+
+// ..3
+let temp3 = [-1,2,3,56,6,-93,0,4].map(el => el*2)
+console.log(temp3)
+
+// ..4
+let temp4 = [10,13,44,55,89,0,12,1].filter(el => el%2 == 0)
+console.log(temp4)
