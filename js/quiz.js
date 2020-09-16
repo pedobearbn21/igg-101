cb69943b77a09dc2d7b5aa9d53730a07a819acf0
pedobearearn <pedobearearn@gmail.com>
2020-09-16 14:58:31 +0700

    quiz

--- js/quiz.js
+++ js/quiz.js
@@ -3,7 +3,7 @@ for (let index = 1; index <= 20; index++) {
 }
 
 for (let index = 1; index <= 20; index++) {
-    if( index % 2 == 0 ){
+    if( index % 2 === 0 ){
         console.log(index)
     }
 }
@@ -11,5 +11,5 @@ for (let index = 1; index <= 20; index++) {
 let temp1 = [-1,2,3,56,6,-93,0,4].map(el => el * 2 )
 console.log(temp1)
 
-let temp2 = [10,13,44,55,89,0,12,1].filter(el => el%2== 0)
+let temp2 = [10,13,44,55,89,0,12,1].filter(el => el%2 === 0)
 console.log(temp2)
